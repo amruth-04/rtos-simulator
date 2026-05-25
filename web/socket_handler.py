f70927c8bb4baf76ce0d69021db765f9bf83a6d3
amruth-04 <amruthshirin@gmail.com>
2026-05-25 23:21:24 +0530

    fix: socketio sleep and transport config

--- web/socket_handler.py
+++ web/socket_handler.py
@@ -8,108 +8,47 @@ from scheduler.simulator import (
 
 def run_simulation(socketio, algorithm):
 
-    # Select scheduling algorithm
-
     if algorithm == "Round Robin":
-
         simulation = round_robin_simulation()
-
     elif algorithm == "FCFS":
-
         simulation = fcfs_simulation()
-
     elif algorithm == "SJF":
-
         simulation = sjf_simulation()
-
     else:
-
         simulation = priority_simulation()
 
-    # Metrics
-
     completed_tasks = 0
-
     context_switches = 0
-
     cpu_utilization = 0
-
     previous_task = None
-
-    # Run simulation
 
     for task_name, state in simulation:
 
-        # Context switch tracking
-
         if previous_task != task_name:
-
             context_switches += 1
-
         previous_task = task_name
 
-        # Completed task tracking
-
         if state == "FINISHED":
-
             completed_tasks += 1
 
-        # Fake CPU utilization
+        cpu_utilization = min(cpu_utilization + 10, 100)
 
-        cpu_utilization = min(
+        socketio.emit("task_update", {
+            "name": task_name,
+            "state": state
+        })
 
-            cpu_utilization + 10,
+        socketio.emit("timeline_update", {
+            "task": task_name
+        })
 
-            100
-        )
-
-        # Task state update
-
-        socketio.emit(
-
-            "task_update",
-
-            {
-
-                "name": task_name,
-
-                "state": state
-            }
-        )
-
-        # IMPORTANT FIX:
-        # Send FULL task name
-
-        socketio.emit(
-
-            "timeline_update",
-
-            {
-
-                "task": task_name
-            }
-        )
-
-        # Metrics update
-
-        socketio.emit(
-
-            "metrics_update",
-
-            {
-
-                "cpu_utilization":
-                    f"{cpu_utilization}%",
-
-                "context_switches":
-                    context_switches,
-
-                "completed_tasks":
-                    completed_tasks,
-
-                "algorithm":
-                    algorithm
-            }
-        )
+        socketio.emit("metrics_update", {
+            "cpu_utilization": f"{cpu_utilization}%",
+            "context_switches": context_switches,
+            "completed_tasks": completed_tasks,
+            "algorithm": algorithm
+        })
 
         print(task_name, state)
+
+        socketio.sleep(1)
